38e88a981242af8d38406c50621c03cd8802a752
Sarah Bhaskaran <sbhaskaran1357@berkeley.edu>
2022-01-03 11:05:11 -0800

    worked on these files here for other branch

---
+++ scripts/args.py
@@ -0,0 +1,45 @@
+import argparse
+import re
+
+PLATOON_PRESETS= {
+    # scenario 1: 4 AVs with human cars inbetween, some of which are sensing cars used to collect metrics on
+    'scenario1': 'human#sensor human*5 (human#sensor human*5 av human*5)*4 human#sensor human*5 human#sensor',
+}
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--platoon', type=str, default='av human*5',
+        help='Platoon of vehicles following the leader. Can contain either "human"s or "av"s. '
+             '"(av human*2)*2" can be used as a shortcut for "av human human av human human". '
+             'Vehicle tags can be passed with hashtags, eg "av#tag" "human#tag*3". '
+             'Available presets: "scenario1".')
+    parser.add_argument('--gui', default=False, action='store_true',
+        help='Run in gui mode so that the platoon\'s progress can be watched during simulation. '
+            'Has not been tested.')
+    args = parser.parse_args()
+    args.platoon = platoon_parse(args.platoon)
+
+# From trajectory-training
+def platoon_parse(platoon):
+    if platoon in PLATOON_PRESETS:
+        print(f'Setting scenario preset "{platoon}"')
+        platoon = PLATOON_PRESETS[platoon]
+
+    # replace (subplatoon)*n into subplatoon ... subplatoon (n times)
+    replace1 = lambda match: ' '.join([match.group(1)] * int(match.group(2)))
+    platoon = re.sub(r'\(([a-z0-9\s\*\#]+)\)\*([0-9]+)', replace1, platoon)
+    # parse veh#tag1...#tagk*n into (veh, [tag1, ..., tagk], n)
+    platoon_lst = re.findall(r'([a-z]+)((?:\#[a-z]+)*)(?:\*?([0-9]+))?', platoon)
+
+    # spawn vehicles
+    vehicles = []
+    for vtype, vtags, vcount in platoon_lst:
+        for _ in range(int(vcount) if vcount else 1):
+            tags = vtags.split('#')[1:]
+            if vtype == 'av':
+                vehicles.append('av')
+            elif vtype == 'human':
+                vehicles.append('idm')
+            else:
+                raise ValueError(f'Unknown vehicle type: {vtype}. Allowed types are "human" and "av".')
+    return vehicles
---
+++ scripts/graph_output.py
@@ -0,0 +1,104 @@
+from matplotlib import pyplot as plt
+import os
+import numpy as np
+
+def graph_output(vehs=None):
+    ''' Graph data in cosim/scripts/data from the last run of demo_node.py
+    '''
+
+    dir = os.path.realpath(os.path.dirname('__file__'))
+    # sumo_log.npy data was added in get_data_debug() of demo_node.py
+    # [t, pos[0], pos[1], vel, self.space_gaps[veh].data, self.rel_vels[veh].linear.x]
+    sumo = np.load(dir + '/data/sumo_log.npy')
+    # Give generic names if the platoon is not provided
+    if vehs == None:
+        vehs = ['veh'+str(i) for i in range(sumo.shape[1])]
+    if len(vehs) != sumo.shape[1]:
+        print(f'{len(vehs)} vehicles in platoon names list, {sumo.shape[1]} in sumo data')
+        vehs = ['veh'+str(i) for i in range(sumo.shape[1])]
+
+    plt.figure()
+    for i in range(sumo.shape[1]):
+        plt.plot(sumo[:, i, 0], sumo[:, i, 1], label=vehs[i])
+    plt.xlabel('time')
+    plt.ylabel('xpos')
+    plt.title('x position vs time')
+    plt.legend()
+    plt.savefig(dir+'/xpos_vs_time.png')
+
+    plt.figure()
+    for i in range(sumo.shape[1]):
+        plt.plot(sumo[:, i, 0], sumo[:, i, 2], label=vehs[i])
+    plt.xlabel('time')
+    plt.ylabel('ypos')
+    plt.title('y position vs time')
+    plt.legend()
+    plt.savefig(dir+'/ypos_vs_time.png')
+
+    plt.figure()
+    for i in range(sumo.shape[1]):
+        plt.plot(sumo[:, i, 0], sumo[:, i, 3], label=vehs[i])
+    plt.xlabel('time')
+    plt.ylabel('vel')
+    plt.legend()
+    plt.title('Velocity vs time')
+    plt.savefig(dir+'/vel_vs_time.png')
+
+    plt.figure()
+    for i in range(sumo.shape[1]):
+        plt.plot(sumo[:, i, 0], sumo[:, i, 4], label=vehs[i])
+    plt.xlabel('time')
+    plt.ylabel('headway (m)')
+    plt.legend()
+    plt.title('Headway vs time')
+    plt.savefig(dir+'/headway_vs_time.png')
+
+    plt.figure()
+    for i in range(sumo.shape[1]):
+        plt.plot(sumo[:, i, 0], sumo[:, i, 4], label=vehs[i])
+    plt.xlabel('time')
+    plt.ylabel('relative velocity (m/s)')
+    plt.legend()
+    plt.title('Relative velocity of preceding car vs time')
+    plt.savefig(dir+'/rel_vel_vs_time.png')
+
+    ros = np.load(dir+'/data/sub_msgs.npy', allow_pickle=True)
+    avs = [veh for veh in vehs if 'av' in veh]
+    if len(avs) != ros.shape[0]:
+        avs = [f'{str(i)}th av in platoon' for i in range(ros.shape[0])]
+
+    plt.figure()
+    for i in range(ros.shape[0]):
+        plt.plot(ros[i, :, 0], ros[i, :, 1], label=avs[i])
+    plt.xlabel('time')
+    plt.ylabel('v_act.linear.x')
+    plt.title('ROS msgs for velocity after accel dynamics')
+    plt.legend()
+    plt.savefig('v_acts_vs_time.png')
+
+    # Same figure? No, another plot with both on same figure
+    plt.figure()
+    cmd_vels = np.load(dir+'/data/cmd_vel_msgs.npy', allow_pickle=True)
+    for i in range(cmd_vels.shape[0]):
+        plt.plot(cmd_vels[i, :, 0], cmd_vels[i, :, 1], label=avs[i]+' cmd_vel')
+    for i in range(ros.shape[0]):
+        plt.plot(ros[i, :, 0], ros[i, :, 1], label=avs[i]+' v_act')
+    plt.xlabel('time')
+    plt.ylabel('velocity')
+    plt.title('ROS msgs for cmd_vels before accel dynamics, v_acts after')
+    plt.legend()
+    plt.savefig('v_acts_cmd_vels_vs_time.png')
+
+    plt.figure()
+    v_refs = np.load(dir+'/data/v_ref_msgs.npy', allow_pickle=True)
+    for i in range(v_refs.shape[0]):
+        plt.plot(v_refs[i, :, 0], v_refs[i, :, 1], label=avs[i])
+    plt.xlabel('time')
+    plt.ylabel('v_ref.linear.x')
+    plt.title('ROS v_ref msgs (predicted acceleration)')
+    plt.legend()
+    plt.savefig('v_refs_vs_time.png')
+
+
+graph_output(['lead', 'av1', 'idm2', 'idm3', 'idm4', 'idm5', 'idm6'])
+# plt.show()
